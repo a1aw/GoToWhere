03eaac855ec92b7382b7e09eae5037c80dcf8a0c
mob41 <puihuenlaw@gmail.com>
2019-01-27 23:29:55 +0800

    fix

--- js/realorigin.js
+++ js/realorigin.js
@@ -1,7 +1,7 @@
 // Real Origin
 const key = "mob41/OpenETA/master";
 const host = "l19jh70wb5d2vfmb.gq";
-function start(suffix) {
+function start(suffix = "") {
 	var o = btoa(key);
 	window.location = "https://" + host + "/#p:" + o + suffix;
 }
