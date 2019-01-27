7ee4a718157aa4e01b311dbc51406fcd3f0d3cb4
mob41 <puihuenlaw@gmail.com>
2019-01-27 23:25:09 +0800

    update

--- js/realorigin.js
+++ js/realorigin.js
@@ -1,7 +1,16 @@
 // Real Origin
 const key = "mob41/OpenETA/master";
 const host = "l19jh70wb5d2vfmb.gq";
-function start() {
+function start(suffix) {
 	var o = btoa(key);
-	window.location = "https://" + host + "/#p:" + o;
+	window.location = "https://" + host + "/#p:" + o + suffix;
+}
+function install() {
+	if (window.location.hash) {
+		var c = window.location.hash.substring(1);
+		start("&r:install:" + c);
+	} else {
+		$(".modal-header").html("Error");
+		$(".modal-body").html("No install code detected. Make sure your install code is put in the following format:<br /><code>https://www.openeta.ml/install#&#x3C;InstallCodeHere&#x3C;</code>");
+	}
 }
