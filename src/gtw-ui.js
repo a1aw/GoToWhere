c9725f04e569662cf977376552920c5c69d2b805
mob41 <puihuenlaw@gmail.com>
2019-11-02 22:20:13 +0800

    Add okay btn

--- src/gtw-ui.js
+++ src/gtw-ui.js
@@ -867,6 +867,9 @@ export var scripts = {
     "updated": function (ver) {
         $("#updated-header").html($.i18n("updated-header", ver));
         $("#updated-desc").html($.i18n("updated-desc", ver));
+        $("#updated-okay-btn").on("click", function () {
+            hideModal();
+        });
         import("./changelog.txt").then(function (mod) {
             $("#updated-changelog").html(mod.default);
         });
