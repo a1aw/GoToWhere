a24ea8619a0b6a0f9aca123517f0282ae8f8f528
mob41 <puihuenlaw@gmail.com>
2019-11-02 21:53:34 +0800

    Update to 1.0.4

---
+++ dist/13.dc225af2a5d6d90efd18.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{88:function(p,n){p.exports="<p data-i18n=plugin-closed-api-warning></p> <p data-i18n=plugin-closed-api-confirm-message></p>"}}]);
+//# sourceMappingURL=13.dc225af2a5d6d90efd18.js.map
